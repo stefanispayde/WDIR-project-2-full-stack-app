e7ea21c085f62c969f86448770573b63a599ec5d
stefani spayde <spayde.stefani@gmail.com>
2017-08-08 09:54:07 -0500

    added entry of post to schema, corrected errors on edit/show page

--- project/models/posts.js
+++ project/models/posts.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
   tag: String,
   title: String,
-  image: String
+  image: String,
+  entry: String
 });
 
 const Post = mongoose.model('Post', postSchema);
