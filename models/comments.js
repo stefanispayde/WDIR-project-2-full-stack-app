7e2cb90b43d69d84e05f625e0e0dfa4ab74889d4
stefani spayde <spayde.stefani@gmail.com>
2017-08-10 14:53:46 -0500

    pushing to heroku

--- models/comments.js
+++ models/comments.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const commentSchema = mongoose.Schema({
   entry: String
+  // title: String
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
