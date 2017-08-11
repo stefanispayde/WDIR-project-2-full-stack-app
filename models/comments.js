41c1ec2dc70992f2d818da60cb2c73b441aaf928
stefani spayde <spayde.stefani@gmail.com>
2017-08-11 07:25:28 -0500

    cleaned up code

--- models/comments.js
+++ models/comments.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-// const Post = require('../models/posts.js');
 
 const commentSchema = mongoose.Schema({
   entry: String
-  // title: String
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
