767ec0a4878a3e893dc1b696253ea8d1e709e23c
stefani spayde <spayde.stefani@gmail.com>
2017-09-17 17:27:12 -0500

    cleaning up CSS, fixed broken show.ejs page

--- controllers/comments.js
+++ controllers/comments.js
@@ -24,7 +24,7 @@ router.post('/', (req, res)=> {
     Comment.create(req.body, (err, createdComment)=> {
       foundPost.comments.push(createdComment);
       foundPost.save((err, data)=> {
-        res.redirect('/posts');   //changed route, initially redirected back to comments/new, want to redirect back to that posts show page which is brokem
+        res.redirect('/posts');  
       });
     });
   });
